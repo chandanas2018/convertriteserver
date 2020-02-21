ccdc23a1c04fcff8cebc5d458832edf2f9b03e88
nianderph <phaneendra.ss@gmail.com>
2020-02-21 14:49:46 +0530

    added set up data

--- app/Helpers/SetupdataHelpers.js
+++ app/Helpers/SetupdataHelpers.js
@@ -43,6 +43,87 @@ class SetupDataHelper{
             }
            
         }
+
+        if(entity.toUpperCase() == "GRADES"){
+            const GradeData = await Database.connection('oracledb').select('*').from('GRADES');
+            var gradeExists =  GradeData.some(grade => grade.GRADEID == dataEntity.GradeId);
+
+            if(gradeExists){
+                return new Promise((resolve,reject)=>{
+                    resolve(null);
+                });
+            }
+            else{
+                var obj = { CATEGORYCODE:dataEntity.CategoryCode,
+                            //EFFECTIVEENDDATE:dataEntity.EffectiveEndDate,
+                            //EFFECTIVESTARTDATE: dataEntity.EffectiveStartDate,
+                            GRADECODE:dataEntity.GradeCode,
+                            GRADEID: dataEntity.GradeId,
+                            GRADENAME: dataEntity.GradeName
+                            };
+    
+                    return new Promise((resolve,reject)=>{
+                        resolve(obj);
+                    });
+            }
+           
+        }
+
+        //Organizations
+        if(entity.toUpperCase() == "ORGANIZATIONS"){
+            const OrgData = await Database.connection('oracledb').select('*').from('ORGANIZATIONS');
+            var OrgExists =  OrgData.some(org => org.ORGANIZATIONID == dataEntity.OrganizationId);
+
+            if(OrgExists){
+                return new Promise((resolve,reject)=>{
+                    resolve(null);
+                });
+            }
+            else{
+                var obj = { NAME:dataEntity.Name,
+                            //EFFECTIVEENDDATE:dataEntity.EffectiveEndDate,
+                            //EFFECTIVESTARTDATE: dataEntity.EffectiveStartDate,
+                            CODE:dataEntity.OrganizationCode,
+                            CLASSIFICATION_CODE: dataEntity.ClassificationCode,
+                            ORGANIZATIONID: dataEntity.OrganizationId,
+                            LOCATIONID: dataEntity.LocationId
+                            };
+    
+                    return new Promise((resolve,reject)=>{
+                        resolve(obj);
+                    });
+            }
+           
+        }
+
+        //Jobs
+        if(entity.toUpperCase() == "JOBS"){
+            const JobData = await Database.connection('oracledb').select('*').from('JOBS');
+            var jobExists =  JobData.some(job => job.JOBID == dataEntity.JobId);
+
+            if(jobExists){
+                return new Promise((resolve,reject)=>{
+                    resolve(null);
+                });
+            }
+            else{
+                var obj = { JOBID:dataEntity.JobId,
+                            //EFFECTIVEENDDATE:dataEntity.EffectiveEndDate,
+                            //EFFECTIVESTARTDATE: dataEntity.EffectiveStartDate,
+                            JOBCODE:dataEntity.JobCode,
+                            JOBFAMILYID: dataEntity.JobFamilyId,
+                            JOBFUNCTIONCODE: dataEntity.JobFunctionCode,
+                            MANAGERLEVEL: dataEntity.ManagerLevel,
+                            NAME: dataEntity.Name,
+                            GRADELADDERID: dataEntity.GradeLadderId
+                            };
+    
+                    return new Promise((resolve,reject)=>{
+                        resolve(obj);
+                    });
+            }
+           
+        }
     }
 }
 
